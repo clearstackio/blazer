b6e3334fb974ce987b898b07c6288d1d1f96a94e
Pandu M <pandu.signin@gmail.com>
2020-07-30 17:57:53 +0530

    added created_at to home index

--- app/models/concerns/blazer/name_validator.rb
+++ app/models/concerns/blazer/name_validator.rb
@@ -1,21 +1,21 @@
 module Blazer
-    module NameValidator
-      extend ActiveSupport::Concern
+  module NameValidator
+    extend ActiveSupport::Concern
 
-      included do
-        before_validation :strip_unwanted_whitespaces
+    included do
+      before_validation :strip_unwanted_whitespaces
 
-        validates :name, presence: true,
-          uniqueness: {
-            scope: :creator_id,
-            case_sensitive: false,
-            message: "already taken for this user."
-          }
-      end
-
-      private
-        def strip_unwanted_whitespaces
-          self.name = "#{self.name}".strip
-        end
+      validates :name, presence: true,
+        uniqueness: {
+          scope: :creator_id,
+          case_sensitive: false,
+          message: "already taken for this user."
+        }
     end
+
+    private
+      def strip_unwanted_whitespaces
+        self.name = "#{self.name}".strip
+      end
   end
+end
